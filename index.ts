c7e27d5e2909ba50238aee69276145f8a671cea8
Oh YoungJe <pine7420@naver.com>
2025-02-12 17:16:00 +0900

    Hotfix: cors 허용

--- index.ts
+++ index.ts
@@ -4,9 +4,20 @@ import { loginRouter } from '#router/loginRouter'
 import { memberRouter } from '#router/memberRouter'
 import { mockRouter } from '#router/mockRouter'
 import { testRouter } from '#router/testRouter'
+import cors from 'cors'
+import { configDotenv } from 'dotenv'
 import express from 'express'
+configDotenv()
 
 const app = express()
+app.use(
+  cors({
+    origin: process.env.FRONTEND_SERVER_URL, // 허용할 출처
+    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'], // 모든 HTTP 메소드 명시
+    credentials: true, // 쿠키 전송 허용 여부
+  }),
+)
+
 app.use(express.json())
 app.use('/member', memberRouter)
 app.use('/login', loginRouter)
