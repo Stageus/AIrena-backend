8dcad0310f4e42189ce969487dc95b4e80244912
taegyun <xorbskim6105@gmail.com>
2025-02-14 16:40:20 +0900

    [SUS-46] Feat/request_params_edit

--- src/dto/frontend/request/FindPasswordRequest.ts
+++ src/dto/frontend/request/FindPasswordRequest.ts
@@ -1,17 +1,14 @@
 interface FindPasswordRequestParams {
   id: string
   email: string
-  token: string
 }
 
 export default class FindPasswordRequest {
   public id: string
   public email: string
-  public token: string
 
   constructor(params: FindPasswordRequestParams) {
     this.id = params.id
     this.email = params.email
-    this.token = params.token
   }
 }
