3ae8fd9cbaea49398cc3a4ce35003b6a95f496d0
taegyun <xorbskim6105@gmail.com>
2025-02-16 17:18:46 +0900

    [SUS-46] Refactor/logout_seperate

--- src/member/router/index.ts
+++ src/member/router/index.ts
@@ -1,9 +1,8 @@
-import controller from '#controller'
 import express from 'express'
-import MemberService from 'src/member/service/MemberService.js'
 import { changeRouter } from './changeRouter.js'
 import { findRouter } from './findRouter.js'
 import { loginRouter } from './loginRouter.js'
+import { logoutRouter } from './logoutRouter.js'
 import { signupRouter } from './signupRouter.js'
 export const memberRouter = express.Router()
 
@@ -11,17 +10,4 @@ memberRouter.use('/login', loginRouter)
 memberRouter.use('/signup', signupRouter)
 memberRouter.use('/find', findRouter)
 memberRouter.use('/change', changeRouter)
-
-/** 로그아웃 API */
-memberRouter.get(
-  '/logout',
-  controller(
-    null,
-    null,
-    null,
-    null,
-  )(async (req, res) => {
-    await MemberService.logout(res)
-    return res.sendStatus(200)
-  }),
-)
+memberRouter.use('/logout', logoutRouter)
