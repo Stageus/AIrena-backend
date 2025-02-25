8b4e6e1885a428f5ab95e9c59d690b4721e9b9bd
taegyun <xorbskim6105@gmail.com>
2025-02-25 22:29:09 +0900

    [SUS-66]rank_query_added

--- src/rank/repository/rankListRepository.ts
+++ src/rank/repository/rankListRepository.ts
@@ -6,7 +6,22 @@ export default class RankListRepository {
     nicknameToSearch: string,
     rangeToGet: string,
   ) {
-    return await postgres.query('SELECT * FROM member')
+    return await postgres.query(`
+      SELECT RANK() OVER(ORDER BY score DESC) AS rank,
+      CASE 
+        WHEN score BETWEEN 0 AND 6 THEN 'DIAMOND'
+        WHEN score BETWEEN 6 AND 12 THEN 'PLATINUM'
+        WHEN score BETWEEN 12 AND 30 THEN 'GOLD'
+        WHEN score BETWEEN 30 AND 50 THEN 'SILVER'
+        WHEN score BETWEEN 50 AND 100 THEN 'BRONZE'
+      END AS tier,
+      nickname,
+      score
+      FROM member
+      WHERE rank < 10
+      LIMIT 10
+
+      `)
   }
 
   static async getFilteredRankListFromDb(nicknameToSearch: string) {
