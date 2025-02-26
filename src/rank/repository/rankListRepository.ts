a49b212ed69283ad7c0723497dd7e0757c30f40c
taegyun <xorbskim6105@gmail.com>
2025-02-26 20:23:25 +0900

    [SUS-66]Feat/filter_query_fix

--- src/rank/repository/rankListRepository.ts
+++ src/rank/repository/rankListRepository.ts
@@ -7,8 +7,8 @@ export default class RankListRepository {
         `
       WITH ranked_members AS (
         SELECT 
-          RANK() OVER(ORDER BY score DESC, created_at ASC) AS rank, -- 점수 기준으로 순위를 매기고, 동점일 경우 오래된 생성 일자가 우선
-          COUNT(*) OVER() AS total_count, -- 전체 사용자 수 계산
+          RANK() OVER(ORDER BY score DESC, created_at ASC) AS rank, 
+          COUNT(*) OVER() AS total_count, 
           nickname,
           score,
           created_at
@@ -17,7 +17,7 @@ export default class RankListRepository {
       SELECT 
         rank,
         CASE 
-          WHEN total_count <= 10 THEN -- 사용자 수가 10명 이하인 경우
+          WHEN total_count <= 10 THEN
             CASE 
               WHEN rank = 1 THEN 'DIAMOND'
               WHEN rank = 2 THEN 'PLATINUM'
@@ -25,12 +25,12 @@ export default class RankListRepository {
               WHEN rank BETWEEN 6 AND 8 THEN 'SILVER'
               ELSE 'BRONZE'
             END
-          WHEN score = 0 THEN 'BRONZE' -- 모든 점수가 0인 경우 'BRONZE'로 할당
-          WHEN rank BETWEEN 1 AND total_count * 0.06 THEN 'DIAMOND'  -- 상위 6%
-          WHEN rank BETWEEN total_count * 0.06 + 1 AND total_count * 0.12 THEN 'PLATINUM' -- 상위 6~12%
-          WHEN rank BETWEEN total_count * 0.12 + 1 AND total_count * 0.30 THEN 'GOLD' -- 상위 12~30%
-          WHEN rank BETWEEN total_count * 0.30 + 1 AND total_count * 0.50 THEN 'SILVER' -- 상위 30~50%
-          ELSE 'BRONZE' -- 나머지 (하위 50%)
+          WHEN score = 0 THEN 'BRONZE'
+          WHEN rank BETWEEN 1 AND total_count * 0.06 THEN 'DIAMOND'  
+          WHEN rank BETWEEN total_count * 0.06 + 1 AND total_count * 0.12 THEN 'PLATINUM'
+          WHEN rank BETWEEN total_count * 0.12 + 1 AND total_count * 0.30 THEN 'GOLD' 
+          WHEN rank BETWEEN total_count * 0.30 + 1 AND total_count * 0.50 THEN 'SILVER'
+          ELSE 'BRONZE' 
         END AS tier,
         nickname,
         score
@@ -48,43 +48,52 @@ export default class RankListRepository {
     return (
       await postgres.query(
         `      
-      WITH ranked_members AS (
-      SELECT 
-        RANK() OVER(ORDER BY score DESC, created_at ASC) AS rank,
-        COUNT(*) OVER() AS total_count, 
-        nickname,
-        score,
-        created_at
-      FROM member_test
+      WITH ranked_members AS ( 
+        SELECT  
+          RANK() OVER(ORDER BY score DESC, created_at ASC) AS rank, 
+          COUNT(*) OVER() AS total_count,  
+          nickname, 
+          score, 
+          created_at 
+        FROM member_test 
+      ), 
+      ranked_with_tiers AS ( 
+        SELECT  
+          rank, 
+          CASE  
+            WHEN total_count <= 10 THEN  
+              CASE  
+                WHEN rank = 1 THEN 'DIAMOND' 
+                WHEN rank = 2 THEN 'PLATINUM' 
+                WHEN rank BETWEEN 3 AND 5 THEN 'GOLD' 
+                WHEN rank BETWEEN 6 AND 8 THEN 'SILVER' 
+                ELSE 'BRONZE' 
+              END 
+            WHEN score = 0 THEN 'BRONZE' 
+            WHEN rank BETWEEN 1 AND total_count * 0.06 THEN 'DIAMOND' 
+            WHEN rank BETWEEN total_count * 0.06 + 1 AND total_count * 0.12 THEN 'PLATINUM' 
+            WHEN rank BETWEEN total_count * 0.12 + 1 AND total_count * 0.30 THEN 'GOLD' 
+            WHEN rank BETWEEN total_count * 0.30 + 1 AND total_count * 0.50 THEN 'SILVER' 
+            ELSE 'BRONZE' 
+          END AS tier, 
+          nickname, 
+          score 
+          FROM ranked_members 
       ),
-      ranked_with_tiers AS (
+      ranked_tiers AS (
         SELECT 
-          rank,
-          CASE 
-            WHEN total_count <= 10 THEN 
-              CASE 
-                WHEN rank = 1 THEN 'DIAMOND'
-                WHEN rank = 2 THEN 'PLATINUM'
-                WHEN rank BETWEEN 3 AND 5 THEN 'GOLD'
-                WHEN rank BETWEEN 6 AND 8 THEN 'SILVER'
-                ELSE 'BRONZE'
-              END
-            WHEN score = 0 THEN 'BRONZE'
-            WHEN rank BETWEEN 1 AND total_count * 0.06 THEN 'DIAMOND'
-            WHEN rank BETWEEN total_count * 0.06 + 1 AND total_count * 0.12 THEN 'PLATINUM'
-            WHEN rank BETWEEN total_count * 0.12 + 1 AND total_count * 0.30 THEN 'GOLD'
-            WHEN rank BETWEEN total_count * 0.30 + 1 AND total_count * 0.50 THEN 'SILVER'
-            ELSE 'BRONZE'
-          END AS tier,
-          nickname,
-          score
-        FROM ranked_members
+          ROW_NUMBER() OVER (ORDER BY score DESC) AS new_rank, 
+          tier, 
+          nickname, 
+          score 
+        FROM ranked_with_tiers 
+        WHERE tier = $1 
       )
       SELECT *
-      FROM ranked_with_tiers
-      WHERE tier = $1 AND rank > $2
-      ORDER BY rank
-      LIMIT 10 
+      FROM ranked_tiers 
+      WHERE new_rank > $2 
+      ORDER BY new_rank 
+      LIMIT 10;
       `,
         [sortType, current],
       )
