b5f8daf909ecc0a1cd18f7c1041feaff52efb979
taegyun <xorbskim6105@gmail.com>
2025-02-28 17:13:18 +0900

    [SUS-52]Feat/regex_change_static_field

--- src/core/util/Regex/index.ts
+++ src/core/util/Regex/index.ts
@@ -1,16 +1,17 @@
 export default class Regex {
-  public id = /^(?=.*[a-zA-Z])[a-zA-Z0-9]{5,16}$/
-  public email = /^[a-zA-Z0-9._%+-]{1,20}@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
-  public nickname = /^(?=.*[가-힣a-zA-Z])[가-힣a-zA-Z0-9]{2,12}$/
-  public title = /^[가-힣a-zA-Z0-9]{2,50}$/
-  public description = /^[가-힣a-zA-Z0-9]{2,500}$/
-  public subject = /^(?=.*[가-힣a-zA-Z])[가-힣a-zA-Z0-9]{2,50}$/
-  public quizCount = /^(10|[1-9])$/
-  public content = /^[가-힣a-zA-Z0-9]{2,500}$/
-  public textAnswer = /^[가-힣a-zA-Z0-9!@#$%^&*()_+={}\[\]:;"'<>,.?~`-]{1,100}$/
-  public search = /^[가-힣a-zA-Z0-9]{2,100}$/
-  public file =
+  public static id = /^(?=.*[a-zA-Z])[a-zA-Z0-9]{5,16}$/
+  public static email = /^[a-zA-Z0-9._%+-]{1,20}@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
+  public static nickname = /^(?=.*[가-힣a-zA-Z])[가-힣a-zA-Z0-9]{2,12}$/
+  public static title = /^[가-힣a-zA-Z0-9]{2,50}$/
+  public static description = /^[가-힣a-zA-Z0-9]{2,500}$/
+  public static subject = /^(?=.*[가-힣a-zA-Z])[가-힣a-zA-Z0-9]{2,50}$/
+  public static quizCount = /^(10|[1-9])$/
+  public static content = /^[가-힣a-zA-Z0-9]{2,500}$/
+  public static textAnswer =
+    /^[가-힣a-zA-Z0-9!@#$%^&*()_+={}\[\]:;"'<>,.?~`-]{1,100}$/
+  public static search = /^[가-힣a-zA-Z0-9]{2,100}$/
+  public static file =
     /^(?:[^\s]+\.jpg|[^\s]+\.png)(,(?:[^\s]+\.jpg|[^\s]+\.png)){0,4}$/
-  public password =
+  public static password =
     /^(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z0-9!@#$%^&*()_+={}\[\]:;"'<>,.?~`-]{8,16}$/
 }
--- src/member/entity/dao/frontend/request/FindIdRequest.ts
+++ src/member/entity/dao/frontend/request/FindIdRequest.ts
@@ -1,6 +1,5 @@
 import ErrorRegistry from '#error/ErrorRegistry'
 import Regex from '#util/Regex'
-const regex = new Regex()
 interface FindIdRequestParams {
   email: string
 }
@@ -11,7 +10,7 @@ export default class FindIdRequest {
   constructor(params: FindIdRequestParams) {
     this.email = params.email
     console.log(this.email)
-    if (!new RegExp(regex.email).test(this.email as string)) {
+    if (!new RegExp(Regex.email).test(this.email as string)) {
       throw ErrorRegistry.INVALID_INPUT_FORMAT
     }
   }
