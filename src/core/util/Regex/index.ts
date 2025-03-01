29f34fbb3f9bddced40dd6e75252d43c83da4dd3
taegyun <xorbskim6105@gmail.com>
2025-03-01 15:57:44 +0900

    [SUS-52]Fea/regex_convetion_edit

--- src/core/util/Regex/index.ts
+++ src/core/util/Regex/index.ts
@@ -1,20 +1,22 @@
 export default class Regex {
-  public static id = /^(?=.*[a-zA-Z])[a-zA-Z0-9]{5,16}$/
-  public static email = /^[a-zA-Z0-9._%+-]{1,20}@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
-  public static nickname = /^(?=.*[가-힣a-zA-Z])[가-힣a-zA-Z0-9]{2,12}$/
-  public static title = /^[가-힣a-zA-Z0-9]{2,50}$/
-  public static description = /^[가-힣a-zA-Z0-9]{2,500}$/
-  public static subject = /^(?=.*[가-힣a-zA-Z])[가-힣a-zA-Z0-9]{2,50}$/
-  public static quizCount = /^(10|[1-9])$/
-  public static content = /^[가-힣a-zA-Z0-9]{2,500}$/
-  public static textAnswer =
+  public static readonly ID = /^(?=.*[a-zA-Z])[a-zA-Z0-9]{5,16}$/
+  public static readonly EMAIL =
+    /^[a-zA-Z0-9._%+-]{1,20}@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
+  public static readonly NICKNAME =
+    /^(?=.*[가-힣a-zA-Z])[가-힣a-zA-Z0-9]{2,12}$/
+  public static readonly TITLE = /^[가-힣a-zA-Z0-9]{2,50}$/
+  public static readonly DESCRIPTION = /^[가-힣a-zA-Z0-9]{2,500}$/
+  public static SUBJECT = /^(?=.*[가-힣a-zA-Z])[가-힣a-zA-Z0-9]{2,50}$/
+  public static QUIZ_COUNT = /^(10|[1-9])$/
+  public static CONTENT = /^[가-힣a-zA-Z0-9]{2,500}$/
+  public static TEXT_ANSWER =
     /^[가-힣a-zA-Z0-9!@#$%^&*()_+={}\[\]:;"'<>,.?~`-]{1,100}$/
-  public static search = /^[가-힣a-zA-Z0-9]{2,100}$/
-  public static tier = /^(DIAMOND|PLATINUM|GOLD|SILVER|BRONZE)$/
-  public static file =
+  public static readonly SEARCH = /^[가-힣a-zA-Z0-9]{2,100}$/
+  public static readonly TIER = /^(DIAMOND|PLATINUM|GOLD|SILVER|BRONZE)$/
+  public static readonly FILE =
     /^(?:[^\s]+\.jpg|[^\s]+\.png)(,(?:[^\s]+\.jpg|[^\s]+\.png)){0,4}$/
-  public static password =
+  public static readonly PASSWORD =
     /^(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z0-9!@#$%^&*()_+={}\[\]:;"'<>,.?~`-]{8,16}$/
-  public static uuid =
+  public static readonly UUID =
     /^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$/
 }
