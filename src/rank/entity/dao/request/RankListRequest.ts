4a8960484eee7035a0f4ff2b929cef6fdc4b8640
Oh YoungJe <pine7420@naver.com>
2025-03-09 15:28:36 +0900

    [SUS-76] tier까지 완료

--- src/rank/entity/dao/request/RankListRequest.ts
+++ src/rank/entity/dao/request/RankListRequest.ts
@@ -1,10 +1,30 @@
+import ErrorRegistry from '#error/ErrorRegistry'
+import Regex from '#util/Regex'
+
 interface RankListRequestParams {
+  tier: string
   current: number
+  nickname: string
 }
 
 export default class RankListRequest {
+  public tier: string
   public current: number
+  public nickname: string
   constructor(params: RankListRequestParams) {
+    if (!params.current) {
+      throw ErrorRegistry.INVALID_INPUT_FORMAT
+    }
     this.current = Number(params.current)
+
+    if (params.tier && !new RegExp(Regex.TIER).test(params.tier)) {
+      throw ErrorRegistry.INVALID_INPUT_FORMAT
+    }
+    this.tier = params.tier
+
+    if (params.nickname && params.nickname.length > 100) {
+      throw ErrorRegistry.INVALID_INPUT_FORMAT
+    }
+    this.nickname = params.nickname
   }
 }
